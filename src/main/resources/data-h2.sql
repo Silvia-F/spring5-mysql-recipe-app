22af28d3082c530942f0d0ce21538d7a223b2bf5
Silvia-F <Silvia-F@users.noreply.github.com>
2020-09-28 10:30:49 +0100

    Refactor database initialization

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,13 @@
+/* This data will only be loaded for the default profile where the datasource.platform is h2 */
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+INSERT INTO unit_of_measure (description) VALUES ('Each');
+INSERT INTO unit_of_measure (description) VALUES ('Dash');
+INSERT INTO unit_of_measure (description) VALUES ('Pint');
